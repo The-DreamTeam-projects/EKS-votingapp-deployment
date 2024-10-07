74d232f803e643a9036fbb93b1a19295a23c26b1
Dominionsys-Inc <mbinuintangku@gmail.com>
2024-10-07 21:29:40 +0100

    Update eks-cluster.tf

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -1,16 +1,3 @@
-resource "aws_eks_cluster" "my_cluster" {
-  name     = var.cluster_name
-  role_arn = "arn:aws:iam::058264135500:role/jenkins-role"
-
-  vpc_config {
-    subnet_ids = aws_subnet.public.*.id
-  }
-
-  tags = {
-    Name = var.cluster_name
-  }
-}
-
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
