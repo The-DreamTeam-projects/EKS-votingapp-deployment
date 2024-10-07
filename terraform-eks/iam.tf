fe1d8dded223ee885804ddb440c6d9d558888f6b
Dominionsys-Inc <mbinuintangku@gmail.com>
2024-10-07 21:57:06 +0100

    Update iam.tf

--- terraform-eks/iam.tf
+++ terraform-eks/iam.tf
@@ -1,12 +1,15 @@
-# IAM Role for EKS Nodes
+resource "aws_iam_role_policy_attachment" "eks_cluster_managed" {
+  role       = "jenkins-role"  # Existing role
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
 resource "aws_iam_role" "eks_node" {
-  name               = "eks-node-role"
+  name = "eks-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -14,41 +17,11 @@ resource "aws_iam_role" "eks_node" {
     ]
   })
 }
-
-# Custom IAM Policy for EKS Node (optional)
-resource "aws_iam_policy" "eks_node_policy" {
-  name        = "eks-node-policy"
-  description = "EKS Node policy"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:DescribeTags",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeVpcs",
-          "logs:DescribeLogStreams",
-          "logs:DescribeLogGroups",
-          "logs:CreateLogStream",
-          "logs:CreateLogGroup",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-# Attach custom IAM policy to the EKS node role (optional)
 resource "aws_iam_role_policy_attachment" "eks_node" {
   role       = aws_iam_role.eks_node.name
   policy_arn = aws_iam_policy.eks_node_policy.arn
 }
 
-# Attach managed IAM policies to the EKS node role
 resource "aws_iam_role_policy_attachment" "eks_node_managed" {
   role       = aws_iam_role.eks_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -56,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "eks_node_managed" {
 
 resource "aws_iam_role_policy_attachment" "eks_node_cni" {
   role       = aws_iam_role.eks_node.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNI_Policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_node_ec2_container_registry" {
